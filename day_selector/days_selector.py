886ee277f49eebdbb05e3ca383f61ee77c576e22
Ariel Voskov <Einstine1984@hotmail.com>
2020-05-25 10:14:10 +0300

    [WIP][NEW] create days selector

---
+++ day_selector/days_selector.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class days_selector:
+    def __init__(self):
+        wanted = {}
+
+    def update_wanted_days(self, person: str, days: List[int]) -> dict:
+        pass
