c4cdd79de82bd2b3ced266e63dbee63aa6755645
Ariel Voskov <Einstine1984@hotmail.com>
2020-05-26 07:18:02 +0300

    [WIP] randomly select people for shifts

--- day_selector/daysselector.py
+++ day_selector/daysselector.py
@@ -1,17 +1,26 @@
+import random
 from typing import List
 
 
 class DaysSelector:
-    def __init__(self):
-        self.wanted = {}
-        self.days = {}
+    DAYS = [1, 2, 3, 4, 5]
+    TEAM = ['Ariel', 'Ofir', 'Harel', 'Udi', 'Josef']
 
-    def update_wanted_days(self, person: str, days: List[int]) -> dict:
+    def __init__(self):
+        self.wanted = {person: self.DAYS for person in self.TEAM}
+        self.days = {day: self.TEAM for day in self.DAYS}
+
+    def update_wanted_days(self, person: str, days: List[int]) -> None:
         self.wanted[person] = days
-        for day in days:
-            self.days[day].append(person)
-        return self.wanted
+        # for day in days:
+        #     self.days[day].append(person)
 
     def distribute_shifts(self):
-        for day in range(1, 6):
-            pass
+        shifts = {}
+        for day in self.DAYS:
+            shifts[day] = random.sample(self.days[day], 3)
+        return shifts
+
+
+ds = DaysSelector()
+print(ds.distribute_shifts())
