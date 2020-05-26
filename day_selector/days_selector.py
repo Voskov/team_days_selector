e75fd60ba5540b49f8ab1a9ba296b16980bb1fe7
Ariel Voskov <Einstine1984@hotmail.com>
2020-05-26 10:15:06 +0300

    [WIP] rename stuff, again

--- day_selector/days_selector.py
+++ day_selector/days_selector.py
@@ -1,2 +1,27 @@
-class days_selector:
-    pass
+import random
+from typing import List
+from pprint import pprint as pp
+
+
+class DaysSelector:
+    DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+    TEAM = ['Ariel', 'Ofir', 'Harel', 'Udi', 'Josef']
+
+    def __init__(self):
+        self.wanted = {person: self.DAYS for person in self.TEAM}
+        self.days = {day: self.TEAM for day in self.DAYS}
+
+    def update_wanted_days(self, person: str, days: List[int]) -> None:
+        self.wanted[person] = days
+        # for day in days:
+        #     self.days[day].append(person)
+
+    def distribute_shifts(self):
+        shifts = {}
+        for day in self.DAYS:
+            shifts[day] = random.sample(self.days[day], 3)
+        return shifts
+
+
+ds = DaysSelector()
+pp(ds.distribute_shifts())
